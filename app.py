717e95e69c5baa7fa0a639743fb717c82a2772ae
alichavoushi <alirezachavoushi@gmail.com>
2024-06-07 20:18:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,41 +9,39 @@ import os
 url="https://raw.githubusercontent.com/alichavoushi/Data-Analysis/main/Trreb%20Analysis%20Toronto_C.csv?token=GHSAT0AAAAAACTJ6SFA3RDVKP5BJISQ2XDUZTCMYRQ"
 
 df = pd.read_csv(url, index_col=0, encoding='ISO-8859-1')
-#df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'Trreb Analysis Toronto_C.csv.csv'), encoding='ISO-8859-1')
-   
-#df = pd.read_csv(r'C:\TRREB ANALYSIS\Trreb Analysis Toronto_C.csv', encoding='ISO-8859-1')
-
-# Read the CSV file
-#df = pd.read_csv(r'C:\TRREB ANALYSIS\Trreb Analysis Toronto_C.csv', encoding='ISO-8859-1')
 
 # Create a DataFrame
 df1 = pd.DataFrame(df)
 df1['Sold Price'] = pd.to_numeric(df1['Sold Price'], errors='coerce')
+
 condition = (df1['Status'] == 'Sold') & (df1['Municipality District'] == 'Toronto C01')& (df1['Sold Price'] < 2000000)
-filtered_selected_columns = df[condition][['Community', 'Bedrooms', 'SqFt', 'Sold Price','DOM']]
+
+filtered_selected_columns = df[condition][['Community', 'Bedrooms', 'SqFt', 'Sold Price','DOM','Exposure']]
 # Convert 'Sold Price' column to numeric
 filtered_selected_columns['Sold Price'] = pd.to_numeric(filtered_selected_columns['Sold Price'], errors='coerce')
 filtered_selected_columns['DOM'] = pd.to_numeric(filtered_selected_columns['DOM'], errors='coerce')
 #filtered_selected_columns.to_csv(r'C:\TRREB ANALYSIS\filtered_selected_columns.csv', encoding='ISO-8859-1')
-grouped_df = filtered_selected_columns.groupby(['Community', 'Bedrooms', 'SqFt']).agg(
+filtered_selected_columns.dropna(subset=['SqFt'], inplace=True)
+filtered_selected_columns['Exposure'].fillna('Unknown', inplace=True)
+filtered_selected_columns['SqFt'] = filtered_selected_columns['SqFt'].astype(str)
+filtered_selected_columns['Community'] = filtered_selected_columns['Community'].astype(str)
+filtered_selected_columns['Exposure'] = filtered_selected_columns['Exposure'].astype(str)
+
+grouped_df = filtered_selected_columns.groupby(['Community', 'Bedrooms', 'SqFt','Exposure']).agg(
     avg_sold_price=('Sold Price', 'mean'),
     avg_DOM=('DOM', 'mean'),
-    count=('Community', 'size')
+    units=('Community', 'size')
 ).reset_index()
+
 
 grouped_df['avg_sold_price'] = np.ceil(grouped_df['avg_sold_price'])
 grouped_df['avg_DOM'] = np.ceil(grouped_df['avg_DOM'])
 
-grouped_df = grouped_df.reset_index().sort_values(by=['Community', 'Bedrooms','SqFt'])
-
-#grouped_df.to_csv(r'C:\TRREB ANALYSIS\Average_Sold_Price.csv', encoding='ISO-8859-1')
-# Create a scatter plot using Plotly
-
-
 # Define slicers (dropdowns) for filtering data
 community_options = [{'label': community, 'value': community} for community in filtered_selected_columns['Community'].unique()]
 bedroom_options = [{'label': str(bedroom), 'value': bedroom} for bedroom in filtered_selected_columns['Bedrooms'].unique()]
-
+sqft_options = [{'label': sqft, 'value': sqft} for sqft in filtered_selected_columns['SqFt'].unique()]
+exposure_options = [{'label': exposure, 'value': exposure} for exposure in filtered_selected_columns['Exposure'].unique()]
 app = Dash(__name__)
 server = app.server
 # Define the layout of the web application
@@ -53,14 +51,32 @@ app.layout = html.Div([
         id='community-filter',
         options=community_options,
         value=[option['value'] for option in community_options],  # Set default value to all communities
-        multi=True  # Allow multiple selections
+        multi=True,  # Allow multiple selections
+        style={'font-size': 'smaller','width': '100%'}
     ),
     html.Label('Select Bedrooms:'),
     dcc.Dropdown(
         id='bedroom-filter',
         options=bedroom_options,
         value=[option['value'] for option in bedroom_options],  # Set default value to all bedrooms
-        multi=True  # Allow multiple selections
+        multi=True,  # Allow multiple selections
+        style={'font-size': 'smaller','width': '100%'}
+    ),
+    html.Label('Select SqFt:'),
+    dcc.Dropdown(
+        id='sqft-filter',
+        options=sqft_options,
+        value=[option['value'] for option in sqft_options],  # Set default value to all SqFt
+        multi=True,  # Allow multiple selections
+        style={'font-size': 'smaller','width': '100%'}
+    ),
+    html.Label('Select Exposure:'),
+    dcc.Dropdown(
+        id='exposure-filter',
+        options=exposure_options,
+        value=[option['value'] for option in exposure_options],  # Set default value to all Exposures
+        multi=True,  # Allow multiple selections
+        style={'font-size': 'smaller','width': '100%'}  # Adjust font size
     ),
     dcc.Graph(id='scatter-plot')
 ])
@@ -69,24 +85,43 @@ app.layout = html.Div([
 @app.callback(
     Output('scatter-plot', 'figure'),
     [Input('community-filter', 'value'),
-     Input('bedroom-filter', 'value')]
+     Input('bedroom-filter', 'value'),
+     Input('sqft-filter', 'value'),
+     Input('exposure-filter', 'value')]
 )
 
-def update_scatter_plot(selected_communities, selected_bedrooms):
-    filtered_df = grouped_df[grouped_df['Community'].isin(selected_communities) & grouped_df['Bedrooms'].isin(selected_bedrooms)]
+def update_scatter_plot(selected_communities, selected_bedrooms, selected_sqft, selected_exposure):
+    filtered_df = grouped_df[grouped_df['Community'].isin(selected_communities) & 
+                             grouped_df['Bedrooms'].isin(selected_bedrooms) & 
+                             grouped_df['SqFt'].isin(selected_sqft) &
+                             grouped_df['Exposure'].isin(selected_exposure)]
+    
+#def update_scatter_plot(selected_communities, selected_bedrooms):
+ #   filtered_df = grouped_df[grouped_df['Community'].isin(selected_communities) & grouped_df['Bedrooms'].isin(selected_bedrooms)]
+    #filtered_df = filtered_df[filtered_df['SqFt'] != 0]
+    def extract_lower_bound(s):
+        try:
+            return int(s.split('-')[0])
+        except ValueError:
+            return 0  # If the value is not convertible to an integer, return 0
+
+
+    sorted_x_values = sorted(filtered_df['SqFt'].unique(), key=extract_lower_bound)
+    
+    
     fig = px.scatter(filtered_df, x='SqFt', y='avg_sold_price', color='Community',
-                     size='count', hover_name='Community', 
+                     size='units',# hover_name='Community',
+                     hover_data=['Bedrooms','Exposure','units','avg_DOM'],
                      labels={'SqFt': 'Square Feet', 'Sold Price': 'Average Sold Price'},
                      title='Average Sold Price by Square Feet and Community')
+    
+    
 
-    fig.update_yaxes(range=[400000, 2000000], tickformat='$,.0f', dtick=100000)
-    
-    fig.update_traces(
-        hovertemplate='<b>%{hovertext}</b><br>SqFt: %{x}<br>Average Sold Price: %{y}<br>Bedrooms: %{customdata[0]}<br>DOM: %{customdata[2]}<br>Count: %{customdata[1]}<extra></extra>',
-        customdata=filtered_df[['Bedrooms', 'count','avg_DOM']]
-    )
+
+    fig.update_yaxes(range=[400000, 2000000], tickformat='$,.0f', dtick=200000)
+    fig.update_xaxes(categoryorder='array', categoryarray=sorted_x_values)
     fig.update_layout(height=600)
-    
+    fig.update_layout(legend=dict(font=dict(size=8))) 
     return fig
 
 if __name__ == '__main__':
