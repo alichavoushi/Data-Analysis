3006de9578b2639e776ad27dad09e9b6ba9edcb5
alichavoushi <alirezachavoushi@gmail.com>
2024-06-25 22:03:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-# app.py
 import dash
 import dash_bootstrap_components as dbc
-from dash import dcc
-from dash import html
+from dash import dcc, html
 from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -13,14 +11,14 @@ app.layout = dbc.Container(
         dbc.Row(
             dbc.Col(
                 dcc.Input(id='input-1', placeholder='Enter text...', type='text', value=''),
-                width={'size': 4, 'offset': 4}
+                width={'size': 4, 'offset': 4}  # Corrected syntax for width
             ),
             justify='center'
         ),
         dbc.Row(
             dbc.Col(
                 html.Div(id='output-1', children=''),
-                width={'size': 4, 'offset': 4}
+                width={'size': 4, 'offset': 4}  # Corrected syntax for width
             ),
             justify='center'
         ),
