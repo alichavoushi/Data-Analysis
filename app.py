4499075cc7bd75809d167f81c0a0117c0c283180
alichavoushi <alirezachavoushi@gmail.com>
2024-06-25 21:58:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # app.py
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
