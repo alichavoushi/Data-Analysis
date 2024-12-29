0a1203fa5c4e67c1b74fcf5516e2b396974e756a
alichavoushi <alirezachavoushi@gmail.com>
2024-12-28 21:55:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ import os
 
 google_api_key = os.getenv('GOOGLE_API_KEY')
 
-#url="https://raw.githubusercontent.com/alichavoushi/Data-Analysis/main/Trreb%20Analysis%20Toronto_C_geo3%20-%20Copy.csv?token=GHSAT0AAAAAACTJ6SFA3RDVKP5BJISQ2XDUZTCMYRQ"
-df = pd.read_csv(r'C:\TRREB ANALYSIS\Trreb Analysis Toronto_C_reyhan.csv', encoding='ISO-8859-1')
-#df = pd.read_csv(url, index_col=0, encoding='ISO-8859-1')
+url="https://raw.githubusercontent.com/alichavoushi/Data-Analysis/main/Trreb%20Analysis%20Toronto_C_geo3%20-%20Copy.csv?token=GHSAT0AAAAAACTJ6SFA3RDVKP5BJISQ2XDUZTCMYRQ"
+#df = pd.read_csv(r'C:\TRREB ANALYSIS\Trreb Analysis Toronto_C_reyhan.csv', encoding='ISO-8859-1')
+df = pd.read_csv(url, index_col=0, encoding='ISO-8859-1')
 
 # Create a DataFrame
 df1 = pd.DataFrame(df)
